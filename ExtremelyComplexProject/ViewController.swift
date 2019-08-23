4e64bd2d995bcd003e961172213b251fe285af4f
Sebastiano Catellani <sebastiano.catellani@secretescapes.com>
2019-08-23 17:25:30 +0200

    Init project

---
+++ ExtremelyComplexProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExtremelyComplexProject
+//
+//  Created by Sebastiano Catellani on 23.08.19.
+//  Copyright © 2019 Sebastiano Catellani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExtremelyComplexProjectTests/ExtremelyComplexProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExtremelyComplexProjectTests.swift
+//  ExtremelyComplexProjectTests
+//
+//  Created by Sebastiano Catellani on 23.08.19.
+//  Copyright © 2019 Sebastiano Catellani. All rights reserved.
+//
+
+import XCTest
+@testable import ExtremelyComplexProject
+
+class ExtremelyComplexProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
